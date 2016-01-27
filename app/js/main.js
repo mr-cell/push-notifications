90af000e2ab427f812d83edd4cf653e2a406d5ba
Marcel Schally - Kacprzak <marcel.schally-kacprzak@lhsystems.com>
2016-01-27 15:25:01 +0100

    finished tutorial

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,49 @@
-'use strict';
+(function() {
+    'use strict';
 
-// TODO
+    var reg;
+    var sub;
+    var isSubscribed = false;
+    var subscribeButton = document.querySelector('button');
+
+    if('serviceWorker' in navigator) {
+        console.log('Service Worker is supported.');
+        navigator.serviceWorker.register('sw.js').then(function(serviceWorkerRegistration) {
+            reg = serviceWorkerRegistration;
+            subscribeButton.disabled = false;
+            console.log(':^)', reg);
+        }).catch(function(err) {
+            console.log(':^(', err);
+        });
+    }
+
+    subscribeButton.addEventListener('click', function() {
+        if(isSubscribed) {
+            unsubscribe();
+        } else {
+            subscribe();
+        }
+    });
+
+    function subscribe() {
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(pushSubscription) {
+            sub = pushSubscription;
+            console.log('Subscribed to endpoint: ', sub.endpoint);
+            subscribeButton.textContent = 'Unsubscribe';
+            isSubscribed = true;
+        });
+    }
+
+    function unsubscribe() {
+        sub.unsubscribe().then(function(event) {
+            subscribeButton.textContent = 'Subscribe';
+            console.log('Unsubscribed!', event);
+            isSubscribed = false;
+        }).catch(function(error) {
+            console.log('Error unsubscribing', error);
+            subscribeButton.textContent = 'Subscribe';
+        });
+    }
+}());
